7dd8297aa16dc5a925ac3ce8f9c698821074727d
MauriceNzai <maurice.nzai@gmail.com>
2023-02-14 11:44:47 +0000

    Add route to retrieve objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,14 @@ def get_status():
     """
     return jsonify({'status': 'OK'})
 
+
+@app_views.route('/stats', method=['GET'], strict_slashes=False)
+def object_status():
+    """
+    Retrieves the number of each object by type
+    """
+    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+                "reviews": 'Review', "states": 'State', "users": 'User'}
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
