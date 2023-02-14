575d8a7fb78e6e571d232c73ef9925313278d356
MauriceNzai <maurice.nzai@gmail.com>
2023-02-14 11:54:17 +0000

    Add Route for Custom 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,13 @@ Entry point to the API to return the status
 from os import getenv
 from flask import Flask, jsonify, Blueprint
 from models import storage
-from api.vi.views import app_views
+from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app.views)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
+CORS(app_views)
 
 
 @app.teardown_appcontext
@@ -17,6 +20,15 @@ def close_db_session(error):
     closses current database session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    """
+    Custom 404 error handler in JSON format
+    """
+    return({'error': 'Not found!'}), 404
+
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
