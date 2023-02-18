989e4931d87cc00c28bf503143a06291e69d69bb
MauriceNzai <maurice.nzai@gmail.com>
2023-02-18 10:35:30 +0000

    Update to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -65,6 +65,8 @@ class BaseModel:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if new_dict.get('password') is not None and show_pswd is False:
+            del new_dict["password"]
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
