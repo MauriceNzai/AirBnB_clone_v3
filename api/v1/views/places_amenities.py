a14a0c2933b752681746bebc45ba1064eecfed7a
MauriceNzai <maurice.nzai@gmail.com>
2023-02-18 10:07:17 +0000

    Fix errors

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -7,43 +7,55 @@ from flask import Flask, make_response, request, jsonify, abort
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
+from models.place import Placce
 from os import getenv
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'])
-def place_allamens(place_id):
+@app_views.route('/places/<string:place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_amenities(place_id):
     """
     Display list of all amenities in a place
     """
     place = storage.get('Place', place_id)
-    if not place:
+    if place is None:
         abort(404)
-    if not place.amenities:
-        return jsonify([])
-    return jsonify([amen.to_dict() for amen in place.amenities])
+    amenities = [amen.to_dict() for amen in place.amenities]
+    return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE', 'POST'])
-def place_amenity(place_id, amenity_id):
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity(place_id, amenity_id):
     """
-    Displays amenity with given id
+    Deletes given amenity from a place
     """
-    amen = storage.get('Amenity', amenity_id)
     place = storage.get('Place', place_id)
-    if not amen or not place:
+    if place is None:
         abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
 
-    if request.method == 'DELETE':
-        if amen not in place.amenities:
-            abort(404)
-        storage.delete(amen)
-        storage.save()
-        return jsonify({}), 200
 
-    if request.method == 'POST':
-        if amen not in place.amenities:
-            place.amenities.append(amen)
-            place.save()
-            return jsonify(amen.to_dict()), 201
-        return jsonify(amen.to_dict()), 200
+@@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+@swag_from('documentation/place_amenity/post.yml', methods=['POST'])
+def post_amenity2(place_id, amenity_id):
+    """ post amenity by id """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity in place.amenities:
+        return (jsonify(amenity.to_dict()), 200)
+    place.amenities.append(obj)
+    storage.save()
+    return (jsonify(amenity.to_dict(), 201))
